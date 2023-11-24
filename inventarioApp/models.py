988f0b8e383704a706f13864862144f306579ffb
javier loncon <javier.l.b95@outlook.com>
2023-11-23 22:35:38 -0300

    Maxsteels añadidos

--- inventarioApp/models.py
+++ inventarioApp/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-#from django.contrib.auth import usuario
+#from django.contrib.auth import Usuario
 # Create your models here.
 
 class Producto(models.Model):
-    IdProducto = models.IntegerField()
-    codigo = models.IntegerField()
-    categoria = models.CharField()
-    nombre = models.CharField()
-    descripcion = models.CharField()
-    stock = models.IntegerField()
-    precio = models.IntegerField()
+    IdProducto = models.IntegerField(max_length=6)
+    codigo = models.IntegerField(max_length=50)
+    categoria = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=255)
+    stock = models.IntegerField(max_length=6)
+    precio = models.IntegerField(max_length=6)
 
 class Usuario(models.Model):
-    nombre = models.CharField()
-    producto = models.ForeignKey(
-    'Producto', on_delete=models.SET_NULL, null=True ) 
+    nombre = models.CharField(max_length=50)
+    producto = models.ForeignKey('Producto', on_delete=models.SET_NULL, null=True) 
